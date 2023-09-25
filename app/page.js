21e956d47e73a63ff5363c18e903e8549552bc01
rod773 <104315743+rod773@users.noreply.github.com>
2023-09-25 14:00:35 +0200

    form

--- app/page.js
+++ app/page.js
@@ -2,9 +2,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-4xl p-4">Expense Tracker</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <h1 className="text-4xl p-4 text-center">Expense Tracker</h1>
+      </div>
+      <div className="bg-slate-800 p-4 rounded-lg">
+        <form className="grid grid-cols-6 items-center text-black">
+          <input
+            className="col-span-3 p-3 border"
+            type="text"
+            placeholder="Enter Item"
+          />
+          <input
+            className="col-span-3  p-3 border"
+            type="number"
+            placeholder="Enter $"
+          />
+          <button type="submit">+</button>
+        </form>
       </div>
     </main>
   );
